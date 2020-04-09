646d189a8a4b540fb54a5a4950523ad213100abf
Migs <mmejares@hotmail.com>
2020-04-09 17:24:12 -0500

    installed inquirer, axios. able to prompt and store user github name

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const questions = [
+// const questions = [
 
-];
+// ];
 
-function writeToFile(fileName, data) {
-}
+// function writeToFile(fileName, data) {
+// }
 
-function init() {
+// function init() {
 
-}
+// }
 
-init();
+// init();
+
+//////////////////////////////////////////////////////////////////////////
+
+const fs = require("fs");
+const inquirer = require("inquirer");
+const axios = require("axios");
+
+//prompt user for their github user name
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "username",
+        message: "What is your Github username?"
+
+    }
+]).then(function(username){
+    //store user name
+    let githubName= JSON.stringify(username.username);
+    console.log(githubName);
+
+    axios
+     .get("curl -u"+ githubName+ "https://api.github.com/user")
+     .then(function(response){
+         console.log(response.data);
+     })
+    
+})
+  
+//use axios to get response from github API by passing stored user name
+
+//manipulate github api response to get data I want
+
+//prompt user other README related questions
+
+//use temporal literals to popluate README
+
+//use fs to generate MD file
